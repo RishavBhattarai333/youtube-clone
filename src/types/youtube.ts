a15c97b8502a211f5fe14e459769979b8bb951f2
Rishav Bhattarai <rishav.iarattahb@gmail.com>
2026-03-16 13:46:12 +0545

    adding videosnippet command

--- src/types/youtube.ts
+++ src/types/youtube.ts
@@ -9,6 +9,8 @@ export interface VideoSnippet {
     default: { url: string; width: number; height: number };
     medium:  { url: string; width: number; height: number };
     high:    { url: string; width: number; height: number };
+    maxres?:  { url: string; width: number; height: number }; 
+    standard?: { url: string; width: number; height: number }; 
   };
 }
 
